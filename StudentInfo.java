0b8964fb7de65d53d34947d70b232e6f1f9ddf40
notanthony <60042005+notanthony@users.noreply.github.com>
2020-05-22 16:18:32 -0400

    First Assignment
    Second assignment overwrote the first ones repository or something like that so heres the first one

---
+++ StudentInfo.java
@@ -0,0 +1,28 @@
+/**
+* Contains a method <code>void display()</code> that all the <code>main</code>
+* methods call to show the student information. Fill the box with your personal
+* information.
+*
+* @author Anthony Zhao and Bradley Liu
+*/
+
+public class StudentInfo {
+
+    /**
+    * Displays the student information: student name, id, section, etc for each
+    * member of the team.
+    */
+
+    public static void display() {
+
+        System.out.println("************************************************************");
+        System.out.println("*                                                          *");
+        System.out.println("*        Anthony Zhao, 300130883, Class C00, Lab C04       *");
+        System.out.println("*        Bradley Liu, 300125419, Class A00, Lab A02        *");
+        System.out.println("*                                                          *");
+        System.out.println("************************************************************");
+        System.out.println();
+
+    }
+
+}
